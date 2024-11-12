c871d0e6bf584b3f3ebc154f8bd8ca7b7b62a5de
Danya524 <167115342+Danya524@users.noreply.github.com>
2024-11-12 22:27:26 +0300

    Add files via upload

--- obsidian-month.py
+++ obsidian-month.py
@@ -1,15 +1,23 @@
+import sys
 import argparse
 def main():
     parser = argparse.ArgumentParser(description= 'new')
     
-    parser.add_argument('input', tupe = str, help = 'вход')
-    parser.add_argument('output', tupe = str, help = 'выход')
-    
-    parser.add_argument('vervose', narsgs = '?', const = True, default = False, help = 'подробный режим') 
-    
+    parser.add_argument('number_of_file', type = int, help = 'вход')
+    parser.add_argument('month', type = str, help = 'выход')
+    parser.add_argument('year', type = str, help = 'da')
+
     args = parser.parse_args()
-    number = args.one
-    month = args.two
-    yaer = args.three
+    number_of_file = args.number_of_file
+    month = args.month
+    year = args.year
+
+    for i in range(30):
+        i+=1
+        with open(f"1{i} keyword2 keyword2.md", "+a"):
+            pass
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 if __name__ == "__main__":
     main()
