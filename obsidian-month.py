ada56be1b90dbf141f742b65749842648fdeed1a
Danya524 <167115342+Danya524@users.noreply.github.com>
2024-11-16 23:09:34 +0300

    Add files via upload

--- obsidian-month.py
+++ obsidian-month.py
@@ -1,23 +1,25 @@
 import sys
 import argparse
 def main():
-    parser = argparse.ArgumentParser(description= 'new')
+        parser = argparse.ArgumentParser(description= 'Generate file')
+            
+        parser.add_argument('number_of_file', type = int, help = 'Number file')
+        parser.add_argument('month', type = str, help = 'Any one of the 12 months')
+        parser.add_argument('year', type = str, help = 'Any year')
+
+        args = parser.parse_args()
+        number_of_file = args.number_of_file
+        month = args.month
+        year = args.year
     
-    parser.add_argument('number_of_file', type = int, help = 'вход')
-    parser.add_argument('month', type = str, help = 'выход')
-    parser.add_argument('year', type = str, help = 'da')
+        if len(sys.argv)==1:
+            parser.print_help(sys.stderr)
+            sys.exit(1)
+        
+        for i in range(30):
+            i+=1
+            with open(f'{i} keyword2 keyword2.md', '+a'):
+                pass
 
-    args = parser.parse_args()
-    number_of_file = args.number_of_file
-    month = args.month
-    year = args.year
-
-    for i in range(30):
-        i+=1
-        with open(f"{i} keyword2 keyword2.md", "+a"):
-            pass
-    if len(sys.argv)==1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
 if __name__ == "__main__":
     main()
